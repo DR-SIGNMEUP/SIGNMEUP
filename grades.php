dbd59eb9339e3878f9bd8dcbd09bbb6835a3f4c9
vvedula <vishnu_vedula@yahoo.co.in>
2013-04-28 14:02:50 -0700

    Working code for grades.php

--- grades.php
+++ grades.php
@@ -12,7 +12,7 @@ session_start();
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">
 </script>
 
-<script>
+<script type="text/javascript">
 var map = new Object(); 
 map["A"] = "4";
 map["A-"] = "3.7";
@@ -71,7 +71,9 @@ font-size:15px;
 			}else{ 
 				echo "<div>".$_SESSION['first_name']." ".$_SESSION['last_name']."";
 				echo "</div>";
-				
+				$cumulativeGrade = 0;
+				$totalCredits = 0;
+				$gpa = 0;
 				while ( $row = mysql_fetch_assoc($result) ){
 					echo "<div class='panel'>";
 					echo "<table class='tablename' border='0'>";
@@ -87,15 +89,46 @@ font-size:15px;
 					echo "<tr>"; 
 					echo "<td>Grade: ".$row['grade']."</td>";
 					echo "</tr>";
-					echo "<tr>"; 
-					echo "<td>GPA: ".$row['gpa']."</td>";
-					echo "</tr>";
 					echo "</table>";
 					echo "</div>";
-					$grade = getGrade($row['grade']);
-					//$grade = "<script type="text/javascript">getGrade(".$row['grade'].");</script>"; 
-					echo $grade;
+					//$grade = getGrade($row['grade']);
+					if($row['grade']=='A')
+						$grade = 4*$row['no_of_credits'];
+					else if($row['grade']=='A-')
+						$grade = 3.7*$row['no_of_credits'];
+					else if($row['grade']=='B+')
+						$grade = 3.3*$row['no_of_credits'];
+					else if($row['grade']=='B')
+						$grade = 3*$row['no_of_credits'];
+					else if($row['grade']=='B-')
+						$grade = 2.7*$row['no_of_credits'];	
+					else if($row['grade']=='C+')
+						$grade = 2.3*$row['no_of_credits'];
+					else if($row['grade']=='C')
+						$grade = 2*$row['no_of_credits'];
+					else
+						$grade = 0*$row['no_of_credits'];
+					$cumulativeGrade+=$grade; 
+					$totalCredits+=$row['no_of_credits'];
 				}	
+				$gpa = $cumulativeGrade/$totalCredits;
+				$roundedGpa = sprintf ("%.2f", $gpa);
+				
+				$sql = "update student_info set gpa='$roundedGpa' where user_id = ".$_SESSION['user_id'];
+    //echo $sql;
+
+				$result = mysql_query($sql);
+				if(!$result){
+					echo 'Could not run query: ' . mysql_error();
+					exit;
+				}
+				else{
+					echo "<table>";
+					echo "<tr>"; 
+					echo "<td>GPA : ".$roundedGpa."</td>";
+					echo "</tr>";
+					echo "</table>";
+				}
 				
 			}
 		?>
